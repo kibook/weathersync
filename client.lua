7bc7590b487d14d0a479253d85845697f61492dc
kibukj <kibuzhj@gmail.com>
2021-10-07 23:28:39 -0400

    Return true when weather is xmas

--- client.lua
+++ client.lua
@@ -255,7 +255,7 @@ local function toggleSync()
 end
 
 exports("isSnowOnGround", function()
-	return snowOnGround
+	return snowOnGround or currentWeather == "xmas"
 end)
 
 AddEventHandler("weathersync:changeWeather", function(weather, transitionTime, permanentSnow)
